28da73508ac5e77e6e9486f8ebd7fc89396672a5
Charlie Kim <dh77dh77@gmail.com>
2021-12-04 20:01:03 +0900

    add movie model and movieListViewController

---
+++ DGV/Data Model/Movie.swift
@@ -0,0 +1,24 @@
+//
+//  Movie.swift
+//  DGV
+//
+//  Created by DaHae Kim on 2021/12/04.
+//
+
+import Foundation
+import UIKit
+
+class Movie {
+    var title: String?
+    var link: String?
+    var image: UIImage?
+    var subtitle: String?
+    var pubDate: String?
+    var director: String?
+    var actor: String?
+    var userRating: String?
+    
+    init() {
+        
+    }
+}
