0dca63bb27a6cc3c239490a45b229f12f96a05b1
Charlie Kim <dh77dh77@gmail.com>
2021-12-06 01:20:40 +0900

    add movie list tableview

---
+++ DGV/Controllers/MovieListCell.swift
@@ -0,0 +1,23 @@
+//
+//  MovieListCell.swift
+//  DGV
+//
+//  Created by DaHae Kim on 2021/12/05.
+//
+
+import UIKit
+
+class MovieListCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- DGV/Controllers/MovieListViewController.swift
+++ DGV/Controllers/MovieListViewController.swift
@@ -9,9 +9,28 @@ import UIKit
 
 class MovieListViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tableView.dataSource = self
+        tableView.delegate = self
     }
 }
 
+extension MovieListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieListCell", for: indexPath) as! MovieListCell
+        return  cell
+    }
+    
+}
+
+extension MovieListViewController: UITableViewDelegate {
+    
+}
